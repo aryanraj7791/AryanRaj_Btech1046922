5537250d28c69da6270d9c6cf3b5bcd68ab8fdbb
Aryan Raj <aryanraj5371@gmail.com>
2026-01-13 22:39:25 +0530

    Updated api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,47 +1,29 @@
-import axios from 'axios';
-
-const getApiBaseURL = () => {
-  if (process.env.REACT_APP_API_URL) {
-    return process.env.REACT_APP_API_URL;
-  }
-  
-  const hostname = window.location.hostname;
-  if (hostname !== 'localhost' && hostname !== '127.0.0.1') {
-    return `http://${hostname}:5000/api`;
-  }
-  
-  return 'http://localhost:5000/api';
-};
-
-const apiBaseURL = getApiBaseURL();
-console.log('API Base URL:', apiBaseURL); 
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: apiBaseURL,
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
